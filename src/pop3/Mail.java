737433b1a4be34251fccf08e4674b2489cbbc55e
Koialle <yoite.hikari@gmail.com>
2017-01-06 13:33:56 +0100

    Added base Mail class

--- src/pop3/Mail.java
+++ src/pop3/Mail.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pop3;
 
 /**
- *
- * @author Epulapp
+ * Class Mail
+ * 
+ * @author Joris BOULAND
+ * @author Tommy CABRELLI
+ * @author Mélanie DUBREUIL
+ * @author Ophélie EOUZAN
  */
-public class Mail {
+public class Mail
+{
+    private boolean read = false;
+    private boolean toDelete = false;
+    private byte[] content;
+    private int contentLength;
     
+    public Mail(byte[] data)
+    {
+        this.content = data;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public boolean isToDelete() {
+        return toDelete;
+    }
+
+    public void setToDelete(boolean toDelete) {
+        this.toDelete = toDelete;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public void setContentLength(int contentLength) {
+        this.contentLength = contentLength;
+    }
 }
