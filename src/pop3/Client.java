ea8f954727c5e46d9289735389729e06c8204141
Koialle <yoite.hikari@gmail.com>
2017-01-10 10:24:53 +0100

    Début connexion client

--- src/pop3/Client.java
+++ src/pop3/Client.java
@@ -1,10 +1,13 @@
 
 package pop3;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,10 +21,16 @@ import java.util.logging.Logger;
  */
 public class Client
 {
+    // Client parameters
     private boolean delete = false;
-    private Socket socket;
     private State state = State.LAUNCHED;
     
+    // TCP connexion
+    private Socket socket;
+    private BufferedInputStream in = null;
+    private BufferedOutputStream out = null;
+    
+    // Error management
     private int code = 0;
     private Exception exception = null;
     private String errorMsg = "";
@@ -44,7 +53,9 @@ public class Client
     public Client(String hostName, int port)
     {
         try {
-            this.socket = new Socket(hostName, port);
+            socket = new Socket(hostName, port);
+            in = new BufferedInputStream(socket.getInputStream());
+            out = new BufferedOutputStream(socket.getOutputStream());
         } catch (IOException ex) {
             if (ex instanceof SocketException) {
             } else if (ex instanceof UnknownHostException) {
@@ -54,9 +65,35 @@ public class Client
         }
     }
     
-    public void getMessage()
+    public void connection()
     {
+        try {
+            String serverMsg = this.readFromServer();
+            String[] serverArray = serverMsg.split(" ");
 
+            String request = "";
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    private String readFromServer() throws IOException
+    {
+        int dataRead , i;
+        ArrayList<Byte> datas = new ArrayList();
+        while (in.available() == 0);
+        while ((dataRead = in.read()) != -1) {
+            byte b = (byte) dataRead;
+            datas.add(b);
+            if (b == -1) break;
+        }
+        byte[] data = new byte[datas.size()];
+        for (i = 0; i < datas.size(); i++) {
+            data[i] = datas.get(i);
+        }
+
+        return new String(data);
     }
 
     public static void main(String[] args) {
@@ -64,6 +101,6 @@ public class Client
         int port = 110;
         
         Client client = new Client(host, port);
-        client.getMessage();
+        client.connection();
     }
 }
