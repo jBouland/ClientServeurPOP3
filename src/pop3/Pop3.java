690ff7c0ace813cc09f3a73e88f6d8d1f62175e9
Epulapp <joris.bouland@gmail.com>
2017-01-10 09:26:11 +0100

    add POP3 constant

---
+++ src/pop3/Pop3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pop3;
+
+/**
+ *
+ * @author Epulapp
+ * The constants with whitespace at the end
+ */
+public class Pop3 {
+    public static String APOP = "APOP ";
+    public static String QUIT = "QUIT ";
+    public static String STAT = "STAT ";
+    public static String DELETE = "DELE ";
+    public static String RETR = "RETR ";
+    public static String RESET = "RSET ";
+    
+}
