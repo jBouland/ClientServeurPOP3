eddbe348ebaae28d224babd0ee38896fae18b8f1
Koialle <yoite.hikari@gmail.com>
2017-01-10 11:40:31 +0100

    Added model classes

--- src/pop3/Pop3.java
+++ src/pop3/Pop3.java
@@ -11,6 +11,8 @@ package pop3;
  */
 public class Pop3
 {
+    public final static String SEPARATOR = " ";
+    
     public final static String OK = "+OK";
     public final static String ERR = "-ERR";
     
@@ -20,4 +22,22 @@ public class Pop3
     public final static String DELETE = "DELE";
     public final static String RETR = "RETR";
     public final static String RESET = "RSET";
+    
+    public enum ResponseType
+    {
+        APOP_OK(2),
+        STAT_OK(3),
+        LIST_OK(3),
+        RETR_OK(4),
+        DELE_OK(4),
+        QUIT_OK(2),
+        ERR(2);
+        
+        private final int nbParts;
+        
+        private MessageType(int nbParts)
+        {
+            this.nbParts = nbParts;
+        }
+    }
 }
---
+++ src/pop3/RequestPop3.java
@@ -0,0 +1,15 @@
+
+package pop3;
+
+/**
+ * Class ResponsePop3
+ * 
+ * @author Joris BOULAND
+ * @author Tommy CABRELLI
+ * @author Mélanie DUBREUIL
+ * @author Ophélie EOUZAN
+ */
+public class RequestPop3
+{
+    
+}
