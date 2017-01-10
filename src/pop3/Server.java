e35574cb30e3319bce46f7b109ae28e1aa35138e
TommyCabrelli <cab.tommy@gmail.com>
2017-01-10 09:40:43 +0100

    Server skeleton done

--- src/pop3/Server.java
+++ src/pop3/Server.java
@@ -5,10 +5,98 @@
  */
 package pop3;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Epulapp
  */
-public class Server {
+public class Server extends Thread {
+
+    public Server() {
+
+        this.start();
+    }
+
+    public void run() {
+        try {
+
+            ServerSocket welcomeSocket = new ServerSocket(1080);
+            while (true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("Connection acceptée !");
+                BufferedInputStream inFromClient = new BufferedInputStream(connectionSocket.getInputStream());
+
+                while (inFromClient.available() > 0) {
+                    System.out.println("Début réception");
+                    byte[] message = new byte[512];
+                    inFromClient.read(message);
+                    String stringifiedMessage = message.toString();
+                    
+                    switch(stringifiedMessage){
+                        case Pop3.APOP :
+                                break;
+                        case Pop3.DELETE :
+                                break;
+                        case Pop3.QUIT :
+                                break;
+                        case Pop3.RESET :
+                                break;
+                        case Pop3.RETR :
+                                break;
+                        case Pop3.STAT :
+                                break;
+                    }
+                }
+                System.out.println("Fin réception");
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
+    private void deleteAction(){
+        
+    }
+    private void quitAction(){
+        
+    }
+    private void resetAction(){
+        
+    }
+    private void retrieveAction(){
+        
+    }
+    private void statAction(){
+        
+    }
+    private void apopAction(){
+        
+    }
+
 }
