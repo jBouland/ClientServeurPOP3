a1577c5ce00584d2080785a445365174f7483987
Koialle <yoite.hikari@gmail.com>
2017-01-10 10:28:07 +0100

    Remove spaces at end of Pop command and Added OK and Err constants

--- src/pop3/Pop3.java
+++ src/pop3/Pop3.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pop3;
 
 /**
- *
- * @author Epulapp
- * The constants with whitespace at the end
+ * Class Pop3
+ * 
+ * @author Joris BOULAND
+ * @author Tommy CABRELLI
+ * @author Mélanie DUBREUIL
+ * @author Ophélie EOUZAN
  */
-public class Pop3 {
-    public final static String APOP = "APOP ";
-    public final static String QUIT = "QUIT ";
-    public final static String STAT = "STAT ";
-    public final static String DELETE = "DELE ";
-    public final static String RETR = "RETR ";
-    public final static String RESET = "RSET ";
+public class Pop3
+{
+    public final static String OK = "+OK";
+    public final static String ERR = "-ERR";
     
+    public final static String APOP = "APOP";
+    public final static String QUIT = "QUIT";
+    public final static String STAT = "STAT";
+    public final static String DELETE = "DELE";
+    public final static String RETR = "RETR";
+    public final static String RESET = "RSET";
 }
