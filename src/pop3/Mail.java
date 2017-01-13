16c2cc30c329fd974f8841ecb7f519a69c3ef8ce
MDHayden <melanedhayden@gmail.com>
2017-01-13 09:36:27 +0100

    traitement des champs du mail

--- src/pop3/Mail.java
+++ src/pop3/Mail.java
@@ -15,17 +15,51 @@ public class Mail
     private boolean toDelete = false;
     private String content;
     private int contentLength;
+    private int messageID;
+    private String from;
+    private String to;
+    private String subject;
+    private String date;
+    private String message;
     
     public Mail(byte[] data)
     {
         this.content = new String(data);
     }
 
-    public Mail(String data)
+    public Mail(String data, int id)
     {
-        // TODO set ID
         this.content = data;
         this.contentLength = data.length();
+        this.setMessageID(id);
+        String lines[] = this.content.split("\\r?\\n");
+        // TODO : Rendre le traitement plus modulaire. Exemple : le message n'est pas à la norme ...
+        for(int i = 0 ; i < lines.length ; i++){
+            if(i < 6){
+               String line[] = lines[i].split(" ");
+                switch(i){
+                    case 0 :
+                        this.setFrom(line[1]);
+                        break;
+                    case 1 :
+                        this.setTo(line[1]);
+                        break;
+                    case 2 :
+                        this.setSubject(line[1]);
+                        break;
+                    case 3 :
+                        this.setDate(line[1]);
+                        break;
+                    default :
+                        break;
+                } 
+            } else if(i >=6 && i < lines.length){
+                String message = this.getMessage();
+                // Verifier que ça n'enlève pas les sauts de ligne
+                String concat = message.concat(lines[i]);
+                this.setMessage(concat);
+            }
+        }
     }
 
     public boolean isRead() {
@@ -58,6 +92,54 @@ public class Mail
 
     public void setContentLength(int contentLength) {
         this.contentLength = contentLength;
+    }
+
+    public int getMessageID() {
+        return messageID;
+    }
+
+    public void setMessageID(int messageID) {
+        this.messageID = messageID;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
     
     @Override
